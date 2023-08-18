4e89b18bec478788eeea543908cc9e48c8eafc31
Chris Dymond <dwarfered@users.noreply.github.com>
2023-08-18 17:45:41 +0800

    Create Connect To Microsoft Graph.ps1

---
+++ azure-automation-account/Connect To Microsoft Graph.ps1
@@ -0,0 +1,44 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Retrieving an Access Token for Microsoft Graph from an Automation Account
+        Managed Identity.
+        
+    .DESCRIPTION
+        Using an Azure Automation Account Managed Identity to get an Access Token to 
+        connect to Microsoft Graph.
+
+    .NOTES
+        AUTHOR: Chris Dymond
+        UPDATED: 18-08-2023
+#>
+
+#region Supporting Functions
+function Get-ManagedIdentityMsGraphAccessToken {
+    [CmdletBinding()]
+    [OutputType([securestring])]
+    param()
+    process {
+        $headers = [System.Collections.Generic.Dictionary[string, string]]::new()
+        $headers.Add("X-IDENTITY-HEADER", $env:IDENTITY_HEADER) 
+        $headers.Add("Metadata", "True") 
+
+        $body = @{resource = 'https://graph.microsoft.com/' } 
+        $params = @{
+            Uri         = $env:IDENTITY_ENDPOINT;
+            Method      = 'POST';
+            Headers     = $headers;
+            ContentType = 'application/x-www-form-urlencoded';
+            Body        = $body;
+        }
+        $graphAccessToken = Invoke-RestMethod @params
+        ConvertTo-SecureString $graphAccessToken.access_token -AsPlainText -Force
+    }
+}
+#endRegion
+
+$accessToken = Get-ManagedIdentityMsGraphAccessToken
+Connect-MgGraph -AccessToken $accessToken | Out-Null
