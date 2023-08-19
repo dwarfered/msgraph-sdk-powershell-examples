841e4017b56e942803b1d1a5708e297c5ae58c0f
Chris Dymond <dwarfered@users.noreply.github.com>
2023-08-19 17:32:47 +0800

    Create Guests-IdentityProtection-AllApps-MediumOrHighRisk-MFA.ps1

---
+++ identity/conditional access/templates/Guests-IdentityProtection-AllApps-MediumOrHighRisk-MFA.ps1
@@ -0,0 +1,38 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Identity.SignIns"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        CA Policy: Guests-IdentityProtection-AllApps-MediumOrHighRisk-MFA
+
+    .DESCRIPTION
+    
+        A CA Policy mandating all Guests with Medium or High Risk perform MFA.
+
+        Applies to both Sign-In Risk and User Risk.
+
+    .NOTES
+        AUTHOR: Chris Dymond
+        UPDATED: 19-08-2023
+#>
+
+$policy = [Microsoft.Graph.PowerShell.Models.MicrosoftGraphConditionalAccessPolicy]::new()
+$policy.DisplayName = 'Guests-IdentityProtection-AllApps-MediumOrHighRisk-MFA'
+$policy.State = 'disabled'
+
+$policy.Conditions.Users.IncludeGuestsOrExternalUsers.ExternalTenants.MembershipKind = 'all'
+$policy.Conditions.Users.IncludeGuestsOrExternalUsers.GuestOrExternalUserTypes = 'internalGuest,b2bCollaborationGuest,b2bCollaborationMember,b2bDirectConnectUser,otherExternalUser,serviceProvider'
+$policy.Conditions.Applications.IncludeApplications = 'All'
+$policy.Conditions.ClientAppTypes = 'all'
+$policy.Conditions.SignInRiskLevels = @('high', 'medium')
+$policy.Conditions.UserRiskLevels = @('high','medium')
+
+$policy.GrantControls.Operator = 'OR'
+$policy.GrantControls.BuiltInControls = 'mfa'
+
+Connect-MgGraph -Scopes @('Policy.ReadWrite.ConditionalAccess') | Out-Null
+
+New-MgIdentityConditionalAccessPolicy -BodyParameter $policy
+
