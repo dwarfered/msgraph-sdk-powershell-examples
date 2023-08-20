f6ad56b43ea2a2c8d00dd58f9ce711b9c00f2054
Chris Dymond <dwarfered@users.noreply.github.com>
2023-08-20 13:26:34 +0800

    Update Policies.ps1

--- identity/conditional access/Policies.ps1
+++ identity/conditional access/Policies.ps1
@@ -1,21 +1,24 @@
-#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
-#Requires -Modules @{ ModuleName="Microsoft.Graph.Identity.SignIns"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Beta.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Beta.Identity.SignIns"; ModuleVersion="2.3.0" }
 
 $ErrorActionPreference = 'stop'
 
 <#
     .SYNOPSIS
-        Get-MgIdentityConditionalAccessPolicy for a list of Conditional Access Policies
+        Get-MgBetaIdentityConditionalAccessPolicy for a list of Conditional Access Policies
+
+        The beta endpoint is used as preview polices may be in use within the tenant and these
+        are not returned by the default v1.0 endpoint.
 
     .DESCRIPTION
     
     .NOTES
         AUTHOR: Chris Dymond
-        UPDATED: 19-08-2023
+        UPDATED: 20-08-2023
 #>
 
 Connect-MgGraph -Scopes @('Policy.Read.All') | Out-Null
 
-$policies = Get-MgIdentityConditionalAccessPolicy -All
+$policies = Get-MgBetaIdentityConditionalAccessPolicy -All
 
 $policies.DisplayName
