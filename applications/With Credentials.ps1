e487114b6f35b19b18331a22d0469928e9686f94
Chris Dymond <dwarfered@users.noreply.github.com>
2023-08-27 22:49:22 +0800

    Create With Credentials.ps1

---
+++ applications/With Credentials.ps1
@@ -0,0 +1,27 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Applications"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Find all application registrations with credentials.
+
+    .DESCRIPTION
+    
+    .NOTES
+        AUTHOR: Chris Dymond
+        UPDATED: 27-08-2023
+#>
+
+$requiredScopes = @('Application.Read.All')
+$currentScopes = (Get-MgContext).Scopes
+if ($currentScopes -match ([string]::Join('|', $requiredScopes)).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
+
+$applications = Get-MgApplication -All -PageSize 999 `
+| Where-Object { $_.KeyCredentials -ne $null -or $_.PasswordCredentials -ne $null }
+
+
+
