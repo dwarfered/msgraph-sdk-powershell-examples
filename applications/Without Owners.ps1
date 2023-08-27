bb21c6f17e4e881d043ec02819ad4f49a6ca2559
Chris Dymond <dwarfered@users.noreply.github.com>
2023-08-27 22:44:22 +0800

    Create Without Owners.ps1

---
+++ applications/Without Owners.ps1
@@ -0,0 +1,36 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Applications"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Find all application registrations without Owners.
+
+    .DESCRIPTION
+    
+    .NOTES
+        AUTHOR: Chris Dymond
+        UPDATED: 27-08-2023
+#>
+
+$ErrorActionPreference = 'stop'
+
+$requiredScopes = @('Application.Read.All')
+$currentScopes = (Get-MgContext).Scopes
+if ($currentScopes -match ([string]::Join('|',$requiredScopes)).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
+
+$params = @{
+    'All'              = $true;
+    'Filter'           = 'Owners/$count eq 0';
+    'CountVariable'    = 'userCount';
+    'ConsistencyLevel' = 'eventual';
+    'PageSize'         = '999';
+}
+
+$applications = Get-MgApplication @params
+
+
+
