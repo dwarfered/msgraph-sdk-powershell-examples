c76177f9b277ffb56904eb382af4603976708562
Chris Dymond <dwarfered@users.noreply.github.com>
2023-09-12 14:40:03 +0800

    Finding SAML application token signing key expiry dates

---
+++ servicePrincipals/With SAML Token Signing Key End Dates.ps1
@@ -0,0 +1,45 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Beta.Applications"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Finding SAML application token signing key expiry dates.
+
+        NOTE - require's the Beta cmdlet.
+
+    .DESCRIPTION
+    
+    .NOTES
+        AUTHOR: Chris Dymond
+        UPDATED: 12-09-2023
+#>
+
+$requiredScopes = @('Application.Read.All')
+$currentScopes = (Get-MgContext).Scopes
+if ($currentScopes -match ([string]::Join('|', $requiredScopes)).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
+
+$params = @{
+    'All'      = $true;
+    'Filter'   = "accountEnabled eq true";
+    'PageSize' = '999';
+    'Select'   = 'preferredTokenSigningKeyEndDateTime,appDisplayName'
+}
+
+$allPrincipals = Get-MgBetaServicePrincipal @params
+
+$samlPrincipals = $allPrincipals | Where-Object { $_.PreferredTokenSigningKeyEndDateTime -ne $null }
+
+$samlPrincipals | Sort-Object PreferredTokenSigningKeyEndDateTime
+| Select-Object AppDisplayName, 
+@{
+    Name       = 'SigningKeyEndAsLocalDateTime'; 
+    Expression = { $_.PreferredTokenSigningKeyEndDateTime.ToLocalTime(); }
+},
+@{
+    Name       = 'State'; 
+    Expression = { if ($_.PreferredTokenSigningKeyEndDateTime.ToLocalTime() -lt (Get-Date)) { 'Expired' } else { 'Active' } }
+}
