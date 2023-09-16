68e83e82bec45681382034b100e59b487d73aa8d
Chris Dymond <dwarfered@users.noreply.github.com>
2023-09-16 11:41:58 +0800

    Update Internals-IdentityProtection-AllApps-HighUserRisk-MFAandPasswordChange.ps1

--- identity/conditional access/templates/Internals-IdentityProtection-AllApps-HighUserRisk-MFAandPasswordChange.ps1
+++ identity/conditional access/templates/Internals-IdentityProtection-AllApps-HighUserRisk-MFAandPasswordChange.ps1
@@ -16,8 +16,8 @@ $ErrorActionPreference = 'stop'
               administrator intervention.
 
     .NOTES
-        AUTHOR: Chris Dymond
-        UPDATED: 20-08-2023
+        AUTHOR: https://github.com/dwarfered/msgraph-sdk-powershell-examples
+        UPDATED: 16-09-2023
 #>
 
 $config = Get-Content -Path ./config.json -Raw | ConvertFrom-Json
