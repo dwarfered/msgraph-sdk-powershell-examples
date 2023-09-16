6b360feb857c2e9f3e6cc34bd37e8c5dc8337877
Chris Dymond <dwarfered@users.noreply.github.com>
2023-09-16 11:20:10 +0800

    Update Add Microsoft Graph App Roles.ps1

--- servicePrincipals/Add Microsoft Graph App Roles.ps1
+++ servicePrincipals/Add Microsoft Graph App Roles.ps1
@@ -10,8 +10,8 @@ $ErrorActionPreference = 'stop'
     .DESCRIPTION
     
     .NOTES
-        AUTHOR: Chris Dymond
-        UPDATED: 29-08-2023
+        AUTHOR: https://github.com/dwarfered/msgraph-sdk-powershell-examples
+        UPDATED: 14-09-2023
 #>
 
 $principalId = '<Principal ObjectId>'
