dfbe407a6c8c58d57e70e1fba805e8867d985377
Chris Dymond <dwarfered@users.noreply.github.com>
2023-09-16 11:41:28 +0800

    Update Policies.ps1

--- identity/conditional access/Policies.ps1
+++ identity/conditional access/Policies.ps1
@@ -13,11 +13,15 @@ $ErrorActionPreference = 'stop'
     .DESCRIPTION
     
     .NOTES
-        AUTHOR: Chris Dymond
-        UPDATED: 20-08-2023
+        AUTHOR: https://github.com/dwarfered/msgraph-sdk-powershell-examples
+        UPDATED: 16-09-2023
 #>
 
-Connect-MgGraph -Scopes @('Policy.Read.All') | Out-Null
+$requiredScopes = @('Policy.Read.All')
+$currentScopes = (Get-MgContext).Scopes
+if ($currentScopes -match ([string]::Join('|',$requiredScopes)).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
 
 $policies = Get-MgBetaIdentityConditionalAccessPolicy -All
 
