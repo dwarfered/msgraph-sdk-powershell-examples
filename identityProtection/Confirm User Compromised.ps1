528261bfa61adff388f92c85db8a8f1a52260c65
Chris Dymond <dwarfered@users.noreply.github.com>
2023-09-16 18:18:18 +0800

    Create Confirm User Compromised.ps1

---
+++ identityProtection/Confirm User Compromised.ps1
@@ -0,0 +1,29 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.3.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Identity.SignIns"; ModuleVersion="2.3.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Confirm one or more riskyUser objects as compromised. 
+        This action sets the targeted user's risk level to high.
+
+    .DESCRIPTION
+    
+    .NOTES
+        AUTHOR: https://github.com/dwarfered/msgraph-sdk-powershell-examples
+        UPDATED: 16-09-2023
+#>
+
+$requiredScopes = @('IdentityRiskyUser.ReadWrite.All')
+$currentScopes = (Get-MgContext).Scopes
+if ($currentScopes -match ([string]::Join('|', $requiredScopes)).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
+
+$userIds = @()
+
+if ($userIds.Count -gt 0) {
+    Confirm-MgRiskyUserCompromised -UserIds $userIds
+}
+
