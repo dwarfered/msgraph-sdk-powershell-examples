311c2e4d586463594c1bdb49b1d09f5773ee24c1
Chris Dymond <dwarfered@users.noreply.github.com>
2023-10-12 21:05:14 +1000

    Create Where Managed Identity.ps1

---
+++ servicePrincipals/Where Managed Identity.ps1
@@ -0,0 +1,32 @@
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Authentication"; ModuleVersion="2.7.0" }
+#Requires -Modules @{ ModuleName="Microsoft.Graph.Applications"; ModuleVersion="2.7.0" }
+
+$ErrorActionPreference = 'stop'
+
+<#
+    .SYNOPSIS
+        Finding all Managed Identity Service Principals.
+    
+    .NOTES
+        AUTHOR: https://github.com/dwarfered/msgraph-sdk-powershell-examples
+        UPDATED: 12-10-2023
+#>
+
+$requiredScopes = @('Application.Read.All')
+$currentScopes = (Get-MgContext).Scopes
+
+if ($null -eq $currentScopes) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+} elseif (($currentScopes -match ([string]::Join('|', $requiredScopes))).Count -ne $requiredScopes.Count) {
+    Connect-MgGraph -Scopes $requiredScopes | Out-Null
+}
+
+$params = @{
+    'All'      = $true;
+    'Filter'   = "servicePrincipalType eq 'ManagedIdentity'";
+    'PageSize' = '999';
+}
+
+$managedIdentities = Get-MgServicePrincipal @params
+
+$managedIdentities | Format-List
